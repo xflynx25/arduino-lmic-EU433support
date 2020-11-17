b584720ec9e187740f0bf7c312c76966feb2189b
dhineshkumarmcci <dhineshkumar@mcci.com>
2020-11-18 00:45:00 +0530

    added pinmap support for 4802

--- src/arduino_lmic_hal_boards.h
+++ src/arduino_lmic_hal_boards.h
@@ -35,6 +35,7 @@ const HalPinmap_t *GetPinmap_Catena4617();
 const HalPinmap_t *GetPinmap_Catena4618();
 const HalPinmap_t *GetPinmap_Catena4630();
 const HalPinmap_t *GetPinmap_Catena4801();
+const HalPinmap_t *GetPinmap_Catena4802();
 const HalPinmap_t* GetPinmap_ttgo_lora32_v1();
 const HalPinmap_t* GetPinmap_heltec_lora32();
 const HalPinmap_t* GetPinmap_Disco_L072cz_Lrwan1();
