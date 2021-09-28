0bd762ea3362c3021e65faf3e90ec3b344de84de
david gauchard <gauchard@laas.fr>
2021-09-28 12:15:23 +0200

    comment: fix typo

--- src/lmic/lmic_channelshuffle.c
+++ src/lmic/lmic_channelshuffle.c
@@ -108,7 +108,7 @@ int LMIC_findNextChannel(
         memcpy(pShuffleMask, pEnableMask, nEntries * sizeof(*pShuffleMask));
         nSet16 = sidewaysSum16(pShuffleMask, nEntries);
     } else {
-        // don't try to skip the last channel becuase it can't be chosen.
+        // don't try to skip the last channel because it can't be chosen.
         lastChannel = -1;
     }
 
